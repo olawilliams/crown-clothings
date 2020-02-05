72051807a2a6dfac317e724e870cfac6a6efc93b
Your Name <you@example.com>
2020-02-05 16:56:57 +0100

    added basic routing to the app using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import Homepage from './pages/Homepage/Homepage-component'
+import { Switch, Route } from "react-router-dom";
+import Homepage from './pages/Homepage/Homepage-component';
 
 import './App.css';
+
+const HatsPage = (props) => {
+  console.log(props)
+ return (<div>
+    <h1>HATSPAGE</h1>
+  </div>)
+}
+
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact  path='/' component={Homepage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch> 
     </div>
   );
 }
