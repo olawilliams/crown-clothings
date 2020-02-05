85c52fd3cb534de1a104a0001d61a9e110fabbb1
Your Name <you@example.com>
2020-02-05 08:41:16 +0100

    broken down the homepage to smaller components and added styles to the menu-item component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Homepage from './components/Homepage/Homepage'
+import Homepage from './pages/Homepage/Homepage-component'
 
 import './App.css';
 
---
+++ src/pages/Homepage/Homepage-component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Directory from '../../components/Directory/Directory-component';
+
+import './Homepage-styles.scss';
+
+const Homepage = () => {
+    return (
+        <div className= 'homepage'>
+           <Directory />
+        </div> 
+    );
+}
+
+export default Homepage;
